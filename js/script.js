e68311816e9b95a891f5c5ee2c1371ddcb69b668
Ângelo <126002318+parrelladev@users.noreply.github.com>
2025-03-05 01:46:05 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function loadComponent(url, elementId) {
-        fetch(url)
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById(elementId).innerHTML = data;
-            })
-            .catch(error => console.error(`Erro ao carregar ${url}:`, error));
-    }
-
-    // Carregar os componentes
-    loadComponent("pages/profile.html", "profile-container");
-    loadComponent("pages/posts.html", "feed");
+    fetch("pages/posts.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("feed").innerHTML = data;
+        })
+        .catch(error => console.error("Erro ao carregar os posts:", error));
 });
